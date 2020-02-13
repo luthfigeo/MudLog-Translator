86dd9e1146c022d9d1cb751586b64bbce4afc7cd
Luthfi <luthfinurul41@gmail.com>
2020-02-13 12:06:16 +0700

    Add files via upload

--- MudLogTranslator.py
+++ MudLogTranslator.py
@@ -2,7 +2,7 @@
 Created on Mon Feb 10 03:29:54 2020
 @author: Luthfi (lsaif.github.com)
 """
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import csv
 import re
 
@@ -10,6 +10,12 @@ with open('litho_dict.csv', newline='') as infile:
     reader = csv.reader(infile)
     next(reader)
     litholist = dict(reader)
+    
+lithodesc=[]
+with open('cuttingdata.csv', newline='') as infile:
+    reader = csv.reader(infile)
+    for row in reader:
+        lithodesc.append(row[0])
   
 def translate(desc,transdict):
      words = desc.split(' ')
@@ -37,6 +43,11 @@ def mudlog_translator():
     return render_template('index.html', result = result)
 
 sample = "Sstone.bn Lt-gry.vf-gr.sbrnd.Fr-cmt.M-srt.ltl-Mtrx.hd. w ltl Fe min mtrx a.a"
+result = [(translate(x,litholist)) for x in lithodesc]
+
+with open("cuttingdata_trans.csv", 'w', newline='') as myfile:
+     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+     wr.writerow(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
